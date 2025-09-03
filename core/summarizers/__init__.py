d3c0a7662d5cb80102209e6acadd44a1396e1105
Lev <rogulev0805@gmail.com>
2025-09-03 23:11:01 +0300

    Инициализатор для алиасов и модулей задач сокращения текстов

---
+++ core/summarizers/__init__.py
@@ -0,0 +1,10 @@
+"""
+Инициализация алиасов и модулей для задачи сокращения текстов.
+"""
+
+
+from .frequency_summarizer import SimpleFrequencySummarizer
+
+__all__ = [
+    "SimpleFrequencySummarizer"
+]
